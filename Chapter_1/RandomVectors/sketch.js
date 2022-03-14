79e472b19808d41957fbd7a3137a710806e2a63a
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-14 03:53:23 -0300

    Exemplo de random vectors.

---
+++ Chapter_1/RandomVectors/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+
+  // let v = createVector(100, 0);
+  let v = p5.Vector.random2D();
+  v.mult(random(50, 100));
+
+  strokeWeight(4);
+  stroke(255, 50);
+  line(0, 0, v.x, v.y);
+}
